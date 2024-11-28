f688c4334958fec44760f1a3e098b48a7949db35
Gaurav shokhanda <gauravshokanda2@gmail.com>
2024-11-28 18:16:35 +0530

    url whitelist

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 // Middleware
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://aitsadmin.netlify.app"],
+  origin: ["http://localhost:3000", "https://aitstax.netlify.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
