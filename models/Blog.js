f086d8b5fe48a3655e95a6efc15440f4ee833ab6
Gagan <gd885973@gmail.com>
2024-12-10 14:32:56 +0530

    add changes in blogs

--- models/Blog.js
+++ models/Blog.js
@@ -12,7 +12,7 @@ const blogSchema = new mongoose.Schema({
     },
     author: {
         type: String,
-        required: true,
+        required: false,
     },
     tags: [String],
     published: {
